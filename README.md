4fefd06e9bf04e3e48681c88eec8d02d5a0d74a5
Amirmasoud Molaei (TAU) <amirmasoudmolaei1374@gmail.com>
2026-01-27 13:42:23 +0200

    Finalize project webpage and assets

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
-# Nerfies
+# Learning to Capture Rocks using an Excavator
 
-This is the repository that contains source code for the [Nerfies website](https://nerfies.github.io).
+This repository hosts the source files for the project webpage associated with the paper:
 
-If you find Nerfies useful for your work please cite:
-```
-@article{park2021nerfies
-  author    = {Park, Keunhong and Sinha, Utkarsh and Barron, Jonathan T. and Bouaziz, Sofien and Goldman, Dan B and Seitz, Steven M. and Martin-Brualla, Ricardo},
-  title     = {Nerfies: Deformable Neural Radiance Fields},
-  journal   = {ICCV},
-  year      = {2021},
+**Learning to Capture Rocks using an Excavator:  
+A Reinforcement Learning Approach with Guiding Reward Formulation**
+
+The webpage presents qualitative results, evaluation scenarios, and visualizations for the proposed reinforcement learning approach applied to a CAT® 365 excavator model in simulation.
+
+🌐 **Live webpage:**  
+https://tau-alma.github.io/rl-rock-capturing-cat365-webpage/
+
+---
+
+## Citation
+If you use or reference this work, please cite the corresponding paper:
+
+```bibtex
+@misc{molaei2025learningcapturerocksusing,
+  title={Learning to Capture Rocks using an Excavator: A Reinforcement Learning Approach with Guiding Reward Formulation},
+  author={Amirmasoud Molaei and Mohammad Heravi and Reza Ghabcheloo},
+  year={2025},
+  eprint={2510.04168},
+  archivePrefix={arXiv},
+  primaryClass={cs.RO},
+  url={https://arxiv.org/abs/2510.04168}
 }
-```
-
-# Website License
-<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,7 @@ var INTERP_BASE = "./static/interpolation/stacked";
 var NUM_INTERP_FRAMES = 240;
 
 var interp_images = [];
+
 function preloadInterpolationImages() {
   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
     var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
@@ -14,65 +15,57 @@ function preloadInterpolationImages() {
 
 function setInterpolationImage(i) {
   var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
+  image.ondragstart = function () { return false; };
+  image.oncontextmenu = function () { return false; };
   $('#interpolation-image-wrapper').empty().append(image);
 }
 
+$(document).ready(function () {
+  // Navbar burger (mobile menu)
+  $(".navbar-burger").click(function () {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+  // -----------------------------
+  // Carousels (init ONCE each)
+  // -----------------------------
 
+  // Evaluation Scenarios carousel (3 visible, no loop)
+  if (document.querySelector('#results-carousel')) {
+    bulmaCarousel.attach('#results-carousel', {
+      slidesToScroll: 1,
+      slidesToShow: 3,
+      loop: false,
+      infinite: false,
+      autoplay: false
     });
+  }
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
+  // Figures carousel (1 visible, looping)
+  if (document.querySelector('#figures-carousel')) {
+    bulmaCarousel.attach('#figures-carousel', {
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      loop: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000
     });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+  }
 
-    bulmaSlider.attach();
+  // -----------------------------
+  // Interpolation slider logic
+  // -----------------------------
+  preloadInterpolationImages();
 
-})
+  $('#interpolation-slider').on('input', function () {
+    setInterpolationImage(this.value);
+  });
+
+  setInterpolationImage(0);
+  $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+
+  // Initialize Bulma sliders (range inputs)
+  bulmaSlider.attach();
+});
